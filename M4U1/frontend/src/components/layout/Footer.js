5d3fa535a197e5165b3800e9fa79d9af583f3916
aminearafa <aminecurriculum@gmail.com>
2022-03-22 00:49:35 +0100

    footer

--- M4U1/frontend/src/components/layout/Footer.js
+++ M4U1/frontend/src/components/layout/Footer.js
@@ -1,7 +1,9 @@
 import React from 'react' ;
 const Footer = (props) => {
     return (
-      <div>Footer</div>
+        <footer>
+        <p>Derechos reservados. Transporte X año</p>
+        </footer>
     );
 }
 

3b881152c943f3cf2503c9da86bc96011e4460c5
aminearafa <aminecurriculum@gmail.com>
2022-03-22 01:32:36 +0100

    nosotrospage

--- M4U1/frontend/src/pages/NosotrosPage.js
+++ M4U1/frontend/src/pages/NosotrosPage.js
@@ -1,8 +1,23 @@
 import React from 'react' ;
 const NosotrosPage = ( props ) => {
     return (
-      <div>NosotrosPage</div>
+       <section className="holder">
+           <div className="historia">
+               <h2>Historia</h2>
+               <p>Some kids pee their name in the snow. Chuck Norris can pee his name into concrete. Chuck Norris does not sleep. He waits. Chuck Norris named his daughter Mercy. The day she was born was the only day Chuck Norris ever had Mercy. The flu gets a Chuck Norris shot every year. Chuck Norris sleeps with a pillow under his gun.When Chuck Norris does a pushup, he’s pushing the Earth down. Chuck Norris’ tears cure cancer. Too bad he has never cried. Chuck Norris tells Simon what to do. Chuck Norris can squeeze orange juice out of a lemon. Chuck Norris does not use spell check. If he happens to misspell a word, Oxford will change the spelling.</p>
+               <p>Some kids pee their name in the snow. Chuck Norris can pee his name into concrete. Chuck Norris does not sleep. He waits. Chuck Norris named his daughter Mercy. The day she was born was the only day Chuck Norris ever had Mercy. The flu gets a Chuck Norris shot every year. Chuck Norris sleeps with a pillow under his gun.When Chuck Norris does a pushup, he’s pushing the Earth down. Chuck Norris’ tears cure cancer. Too bad he has never cried. Chuck Norris tells Simon what to do. Chuck Norris can squeeze orange juice out of a lemon. Chuck Norris does not use spell check. If he happens to misspell a word, Oxford will change the spelling.</p>
+           </div>
+           <h2>Staff</h2>
+           <div className="personas">
+               <div className="persona">
+                   <img src="images/nosotros/nosotrosl.jpg" width="75" alt="Juan"/>
+                   <h5>Chuck Norris</h5>
+                   <h6>Gerente General</h6>
+                   <p>Chuck refers to himself in the fourth person. Chuck Norris plays Jenga with Stonehenge. When Chuck Norris writes, he makes paper bleed. Freddy Krueger has nightmares about Chuck Norris. When Chuck Norris does a pushup, he’s pushing the Earth down.</p>
+                </div>
+            </div>
+        </section>
     );
 } 
 
-export default Page;
+export default NosotrosPage;

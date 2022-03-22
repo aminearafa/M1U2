1cdbc4265c15060a997fb08301c2084718e65425
aminearafa <aminecurriculum@gmail.com>
2022-03-22 01:02:05 +0100

    x

--- M4U1/frontend/src/App.js
+++ M4U1/frontend/src/App.js
@@ -1,15 +1,29 @@
-import Header from './components/layout/Header' ;
-import Nav from './components/layout/Nav' ;
-import Footer from './components/layout/Footer' ;
+import Header from './components/layout/Header';
+import Nav from './components/layout/Nav';
+import Footer from './components/layout/Footer';
+
+import { BrowserRouter as Router,Switch, Route } from "react-router-dom" ;
+
+import ContactoPage from './pages/contactoPage';
+import HomePage from './pages/HomePage';
+import NosotrosPage from './pages/NosotrosPage';
+import NovedadesPage from './pages/NovedadesPage';
 
 function App() {
   return (
-   <div className="App" >
-     <Header></Header>
-     <Nav/>
-     <Footer/>
-   </div>
- );
+    <Router>
+      <Header/>
+      <Nav />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/nosotros" exact component={NosotrosPage} />
+        <Route path="/novedades" exact component={NovedadesPage} />
+        <Route path="/contacto" exact component={ContactoPage} />
+      </Switch>
+      <Footer/>
+    </Router>
+  
+  );
 }
 
 export default App;

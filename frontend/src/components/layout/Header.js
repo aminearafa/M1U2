73976b2e43b670ff433386c3e3f4442294752c8e
aminearafa <aminecurriculum@gmail.com>
2022-03-22 02:53:12 +0100

    cambios

---
+++ frontend/src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react' ;
+const Header = (props) =>{
+    return (
+        <header>
+            <div className="holder">
+                <div className="logo">
+                    <img src="images/logo.png" width="10O" alt="Transportes X" />
+                    <hl>Transportes X</hl>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;

0e55efa43ff51133f020cac17fbae44b688cf0c3
aminearafa <aminecurriculum@gmail.com>
2022-03-22 03:22:55 +0100

    novedades

--- M4U1/frontend/src/pages/NovedadesPage.js
+++ M4U1/frontend/src/pages/NovedadesPage.js
@@ -3,7 +3,7 @@ const NovedadesPage = ( props ) => {
     return (
         <section className="holder">
             <h2>Novedades</h2>
-            <h3>tttulo</h3>
+            <h3>título</h3>
             <h4>subtitulo</h4>
             <p>cuerpo</p>
         </section>

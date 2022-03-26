9cb9773443c297d5a0c261cf6089738cc238c993
aminearafa <aminecurriculum@gmail.com>
2022-03-26 01:48:29 +0100

    U4

--- M4U1-2/backend/routes/index.js
+++ M4U1-2/backend/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('admin/login',{
+    layout:'admin/layout'
+  });
 });
 
 module.exports = router;
